da9b1c22605b7c46d71d71afe1a4e31fbe8627d9
orvilcab <126080124+orvilcab@users.noreply.github.com>
2023-03-21 17:00:50 +0100

    21032023

---
+++ js/exo.js
@@ -0,0 +1,30 @@
+//1 Déclarez une variable "age" avec une valeur de 25.
+let age=25
+
+//2 Déclarez une variable "nom" avec une valeur de "Jean".
+const nom = "jean"
+
+// 3 Créez une variable "somme" et assignez-lui la somme de deux nombres de votre choix.
+let somme = 3+15
+
+// 4 Créez une variable "chaine" qui contient une chaîne de caractères et utilisez la méthode "toUpperCase()" pour la mettre en majuscules.
+let chaine = "mon petit jardin"
+let result = chaine.toUpperCase();
+console.log(result);
+
+// 5 Créez une variable "tableau" qui contient un tableau de nombres et utilisez la méthode "length" pour en trouver la longueur.
+let tableau = [1,2,3,4,5]
+let result = tableau.length;
+console.log(result);
+
+//6 Créez une variable "objet" qui contient un objet avec deux propriétés : "nom" et "age". Utilisez la notation pointée pour accéder à la propriété "nom".
+let objet= {nom: "thierry", age: 25}
+console.log(objet.nom);
+console.log(objet.age);
+
+// 7 Créez une variable "nombre" qui contient un nombre à virgule flottante et utilisez la méthode "toFixed()" pour arrondir le nombre à deux décimales.
+let nombre= 2.3658987899;
+let result= nombre.toFixed(2);
+console.log(result);
+
+// 8 Créez une variable "date" qui contient la date actuelle et utilisez la méthode "getMonth()" pour trouver le mois actuel.
